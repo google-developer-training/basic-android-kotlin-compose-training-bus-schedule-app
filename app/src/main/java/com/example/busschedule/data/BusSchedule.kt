1c8afec7884f3598392347d5b40321b8d267086c
schordas <schordas@google.com>
2022-10-17 21:02:38 -0700

    Initial problem set starter
    Change-Id: Ib0a82aec4ce585c6fd0dd8f37259c6daf8c82f6e

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -16,25 +16,8 @@
 
 package com.example.busschedule.data
 
-import androidx.annotation.NonNull
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-/**
- * Represents a single table in the database. Each row is a separate instance of
- * the BusSchedule class. Each property corresponds to a column.
- * Additionally, an ID is needed as a unique identifier for
- * each row in the database.
- */
-@Entity(tableName = "Schedule")
 data class BusSchedule(
-    @PrimaryKey
     val id: Int,
-    @NonNull
-    @ColumnInfo(name = "stop_name")
     val stopName: String,
-    @NonNull
-    @ColumnInfo(name = "arrival_time")
     val arrivalTimeInMillis: Int
 )
--- app/src/main/java/com/example/busschedule/ui/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/ui/BusScheduleViewModel.kt
@@ -18,30 +18,39 @@ package com.example.busschedule.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.busschedule.BusScheduleApplication
 import com.example.busschedule.data.BusSchedule
-import com.example.busschedule.data.BusScheduleDao
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-/*
- * View model for Bus Schedule
- * contains methods to access Room DB through [busScheduleDao]
- */
-class BusScheduleViewModel(private val busScheduleDao: BusScheduleDao): ViewModel() {
-    // Get full bus schedule from Room DB
-    fun getFullSchedule(): Flow<List<BusSchedule>> = busScheduleDao.getAll()
-    // Get bus schedule based on the stop name from Room DB
+class BusScheduleViewModel(): ViewModel() {
+    // Get example bus schedule from Room DB
+    fun getFullSchedule(): Flow<List<BusSchedule>> = flowOf(
+        listOf(
+            BusSchedule(
+                1,
+                "Example Street",
+                0
+            )
+        )
+    )
+    // Get example bus schedule by stop
     fun getScheduleFor(stopName: String): Flow<List<BusSchedule>> =
-        busScheduleDao.getByStopName(stopName)
+        flowOf(
+            listOf(
+                BusSchedule(
+                    1,
+                    "Example Street",
+                    0
+                )
+            )
+        )
 
     companion object {
         val factory : ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                val application = (this[APPLICATION_KEY] as BusScheduleApplication)
-                BusScheduleViewModel(application.database.busScheduleDao())
+                BusScheduleViewModel()
             }
         }
     }
