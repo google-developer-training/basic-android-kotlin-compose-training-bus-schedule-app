e9812cf661310b6630de6302bc144bfca1f418c0
John Shea <sheajohn@google.com>
2023-05-18 12:02:01 -0400

    Initial main_m3 branch commit

---
+++ app/src/main/java/com/example/busschedule/ui/BusScheduleViewModel.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.busschedule.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.busschedule.BusScheduleApplication
+import com.example.busschedule.data.BusSchedule
+import com.example.busschedule.data.BusScheduleDao
+import kotlinx.coroutines.flow.Flow
+
+/*
+ * View model for Bus Schedule
+ * contains methods to access Room DB through [busScheduleDao]
+ */
+class BusScheduleViewModel(private val busScheduleDao: BusScheduleDao): ViewModel() {
+    // Get full bus schedule from Room DB
+    fun getFullSchedule(): Flow<List<BusSchedule>> = busScheduleDao.getAll()
+    // Get bus schedule based on the stop name from Room DB
+    fun getScheduleFor(stopName: String): Flow<List<BusSchedule>> =
+        busScheduleDao.getByStopName(stopName)
+
+    companion object {
+        val factory : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as BusScheduleApplication)
+                BusScheduleViewModel(application.database.busScheduleDao())
+            }
+        }
+    }
+}
