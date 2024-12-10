36ddc1a46c6fa8d6f72353f4746e2e015fc6c373
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2024-12-10 16:22:28 +0000

    sdk 35 and compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,16 +17,17 @@ plugins {
     id("com.android.application")
     id("com.google.devtools.ksp") version "2.1.0-1.0.29"
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.example.busschedule"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.busschedule"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -54,9 +55,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
     packaging {
         resources {
